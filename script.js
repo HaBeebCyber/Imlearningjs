abcd2040b9e8381a3590c1ebde483479991d62b0
beeby01 <habeebjimoh29@gmail.com>
2024-05-15 10:50:24 +0100

    Changed all my var to let #DreamWithADeadline

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let display = document.getElementById('display')
 function checkResult(){
-    var myInput = Number(document.getElementById('score').value)
+    let myInput = Number(document.getElementById('score').value)
     console.log(myInput);
 
     if (myInput > 0 && myInput < 40){
@@ -16,7 +16,7 @@ function checkResult(){
 //**For loop */
 //**while loop */
 
-for (var i =0; i >= 10; i++ ){
+for (let i =0; i >= 10; i++ ){
     console.log(i)
 }
 
@@ -26,3 +26,10 @@ for (var i =0; i >= 10; i++ ){
 //buzz = no divisible by 5
 //fizz buzz = no divisible by 3 and 5
 //else = not applicable
+
+// concertination
+let food = "Yam"
+let stew = "Peppered snail stew"
+let protein = "beans"
+
+console.log (`I love to eat ${food} garnished with ${stew} and ${protein}`)
